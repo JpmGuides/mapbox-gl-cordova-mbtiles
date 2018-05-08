24fe610651ba0ef3ebfd9b16d4c312ecbe7f3873
Julien Pilet <julien.pilet@gmail.com>
2018-05-08 15:58:16 +0200

    Avoid copying database
    use our modified version of cordova-sqlite-ext.

--- src/mbtiles_source.js
+++ src/mbtiles_source.js
@@ -10,37 +10,13 @@ class MBTilesSource extends VectorTileSource {
         this.db = this.openDatabase(options.path);
     }
 
-    openDatabase(dbLocation) {
-        const dbName = dbLocation.split("/").slice(-1)[0]; // Get the DB file basename
+    openDatabase(dbName) {
         const source = this;
         if ('sqlitePlugin' in self) {
             if('device' in self) {
                 return new Promise(function (resolve, reject) {
-                    if(device.platform === 'Android') {
-                        resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function (dir) {
-                            dir.getDirectory('databases', {create: true}, function (subdir) {
-                                resolve(subdir);
-                            });
-                        }, reject);
-                    } else if(device.platform === 'iOS') {
-                        resolveLocalFileSystemURL(cordova.file.documentsDirectory, resolve, reject);
-                    } else {
-                        reject("Platform not supported");
-                    }
-                }).then(function (targetDir) {
-                    return new Promise(function (resolve, reject) {
-                        targetDir.getFile(dbName, {}, resolve, reject);
-                    }).catch(function () {
-                        return source.copyDatabaseFile(dbLocation, dbName, targetDir)
-                    });
-                }).then(function () {
-                    var params = {name: dbName};
-                    if(device.platform === 'iOS') {
-                        params.iosDatabaseLocation = 'Documents';
-                    } else {
-                        params.location = 'default';
-                    }
-                    return sqlitePlugin.openDatabase(params);
+                    const params = { name: dbName, location: 'default' };
+                    sqlitePlugin.openDatabase(params, resolve, reject);
                 });
             } else {
                 return Promise.reject(new Error("cordova-plugin-device not available. " +
