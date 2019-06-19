e488df385ce0c68c7f6da41e8df8a94e8f1c160a
Julien Pilet <julien.pilet@gmail.com>
2019-06-19 17:15:34 +0200

    Export database API

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import mapboxgl from 'mapbox-gl/src/index'
 
 import offlineMap from './offline_map'
 import language from './openmaptiles-language.js'
+import Database from './database.js'
 
+mapboxgl.Database = Database;
 mapboxgl.OfflineMap = offlineMap;
 language(mapboxgl);
 
